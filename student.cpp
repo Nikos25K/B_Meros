02c56fac56c58d6a0fd0c8522252dee4237c8220
sdi2200133 <126145967+sdi2200133@users.noreply.github.com>
2023-12-17 19:54:11 +0200

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,30 +1,35 @@
 #include "classes.h"
 
-Student::Student() : Person("","","",0), AM(0), ECTS(0){}
+Student::Student() : Person("", "", "", 0), AM(0), ECTS(0), semester(0), subjects({}){}
 
 //basic constructor
-Student::Student(const string in_name, const string in_surname, const string in_mail, 
-const int in_age,const int am, const int ects):
+Student::Student(const string in_name = " ", const string in_surname = " ", const string in_mail = " ", 
+const int in_age = 0, const int am = 0, const int ects = 0, const int sem = 0, const map<Course*, int> sub = {}):
 Person(in_name, in_surname, in_mail, in_age),
-AM(am), ECTS(ects){}
+AM(am), ECTS(ects), semester(sem), subjects(sub){}
 
 //copy contructor
 Student::Student(const Student& student):
-Person(student), AM(student.AM), ECTS(student.ECTS) {}
+Person(student), AM(student.AM), ECTS(student.ECTS), semester(student.semester), subjects(student.subjects) {}
 
 Student::~Student(){}
 
-void Student::set_AM(int am){
-    AM = am;
+int Student::get_ECTS() const{
+    return ECTS;
+}
+int Student::get_AM() const{
+    return AM;
+}
+int Student::get_semester() const{
+    return semester;
 }
 
 void Student::set_ECTS(int ects){
     ECTS = ects;
 }
-
-int Student::get_AM() const{
-    return AM;
+void Student::set_AM(int am){
+    AM = am;
 }
-int Student::get_ECTS() const{
-    return ECTS;
+void Student::set_semester(int sem){
+    semester = sem;
 }
