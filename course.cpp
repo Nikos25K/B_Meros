c19764074ac7490e98e6ad90552cfec2c57bf6b6
sdi2200133 <126145967+sdi2200133@users.noreply.github.com>
2023-12-17 19:28:24 +0200

    Update course.cpp

--- course.cpp
+++ course.cpp
@@ -1,16 +1,16 @@
 #include "classes.h"
 
 Course::Course(): name(""), ECTS(0), mandatory(0), semester(0),
-people({}), passed(0), kophkan(0) {}
+people({}), passed(0), failed(0) {}
 
 Course::Course(const string nam="", const int ects=0, bool mand=0, const int sem=0,
-const vector<Person*> in_people={}, const int in_passed=0, const int in_kophkan=0):
+const vector<Person*> in_people={}, const int in_passed=0, const int in_failed=0):
 name(nam), ECTS(ects), mandatory(mand), semester(sem),
-people(in_people), passed(in_passed), kophkan(in_kophkan) {}
+people(in_people), passed(in_passed), failed(in_failed) {}
 
 //constructor based on Course*
 Course::Course(const Course *ptr) :name(ptr->get_name()), ECTS(ptr->get_ECTS()), mandatory(ptr->is_mandatory()),
-semester(ptr->get_semester()), people(ptr->get_people()),  passed(ptr->get_passed()), kophkan(ptr->get_kophkan()) {}
+semester(ptr->get_semester()), people(ptr->get_people()),  passed(ptr->get_passed()), failed(ptr->get_failed()) {}
 
 Course::~Course(){}
 
@@ -36,8 +36,8 @@ int Course::get_passed() const{
     return passed;
 }
 
-int Course::get_kophkan() const{
-    return kophkan;
+int Course::get_failed() const{
+    return failed;
 }
 
 
@@ -60,8 +60,8 @@ void Course::set_professors(vector<Professor*> in_profs){
 void Course::incr_passed(void){
     passed++;
 }
-void Course::incr_kophkan(void){
-    kophkan++;
+void Course::incr_failed(void){
+    failed++;
 }
 
 Course& Course::operator+=(const Student* in_student){
