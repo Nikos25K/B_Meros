b9bf607e4a11639b49e9c2b8dd4bb1fee1f2c53a
sdi2200133 <126145967+sdi2200133@users.noreply.github.com>
2023-12-17 21:33:54 +0200

    Update person.cpp

--- person.cpp
+++ person.cpp
@@ -1,28 +1,28 @@
 #include "classes.h"
 
-int Person::count=0;
+int Person::count = 0;
 
-Person::Person() : name(""), surname(""), mail(""), age(0){
+Person::Person() : name(""), surname(""), mail(""), age(0), type(""){
     count++;
 }
 
 //constructor based on Person*
 Person::Person(const Person *ptr) :name(ptr->get_name()), surname(ptr->get_surname()), mail(ptr->get_mail()),
-age(ptr->get_age()){
+age(ptr->get_age()), type(ptr->get_type()){
     count++;
 }
 
 //basic constructor
 Person::Person(const string in_name, const string in_surname, const string in_mail,
-const int in_age):
-name(in_name), surname(in_surname), mail(in_mail), age(in_age){
+const int in_age, const string in_type):
+name(in_name), surname(in_surname), mail(in_mail), age(in_age), type(in_type){
     count++;
 }
 
 //copy contructor
 Person::Person(const Person& person):
 name(person.name), surname(person.surname), mail(person.mail), 
-age(person.age){
+age(person.age), type(person.type){
     count++;
 }
 
@@ -46,13 +46,17 @@ int Person::get_age() const{
     return age;
 }
 
+string Person::get_type() const{
+    return type;
+}
+
 
 ostream &operator<<(ostream &str, const Person person){
     str<<"------------------"<<endl;
     str<<"Name: "<<person.name <<" "<<person.surname<<endl;
     str<<"Mail: "<<person.mail<<endl;
     str<<"Age: "<<person.age<<endl;
-    str<<"Subjects: ";
+    str<<"Type: "<<person.type;
     str<<endl<<"------------------"<<endl;
 
     return str;
@@ -70,6 +74,9 @@ istream &operator>>(istream &str, Person &person){
 
     cout<<"Give age:";
     str>>person.age;
+
+    cout<<"Give type:";
+    str>>person.type;
 
     return str;
 }
